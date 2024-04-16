305b6ee5d7039ea4536bba8eea33368808ad8460
Matt <91123443+vordkan@users.noreply.github.com>
2024-04-16 10:29:58 +0200

    aggiunto

---
+++ filtering/build/CMakeFiles/filtering.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/filtering.dir/filtering.cpp.o"
+  "CMakeFiles/filtering.dir/filtering.cpp.o.d"
+  "filtering"
+  "filtering.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/filtering.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
